e038ba9f17d057f4d5b720a683d7fe06228b0f11
everton paixao <everton.paixao16@gmail.com>
2019-06-28 18:05:55 -0300

    create header component

---
+++ src/components/header/style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 30%;
+  height: 100px;
+  margin: 0 26.5%;
+  margin-top: 5%;
+  text-align: left;
+`;
+
+export const Title = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  color: #170c3a;
+  text-align: left;
+  text-transform: uppercase;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 21px;
+  line-height: 22px;
+  font-weight: 500;
+  color: #170c3a;
+`;
